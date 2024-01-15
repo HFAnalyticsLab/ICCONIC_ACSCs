e2890760f37089934cfc12c9e7a5aa4bf84e6daa
Anne Alarilla (tier0) <tier0\anne.alarilla@ec2amaz-ivsmyb.tier0.prd.thf.aws>
2024-01-15 10:22:52 +0000

    change file names

--- calculate _rates.R
+++ calculate _rates.R
@@ -15,13 +15,13 @@ library(THFstyle)
 
 count18plus<-read_csv(here('data', 'age_grp_tab_plus18.csv'))
 count65plus<-read_csv(here('data', 'age_grp_tab_plus65.csv'))
-count18plus4conds<-read_csv(here('data', 'age_grp_tab_plus18_5cond.csv'))
-count65plus4conds<-read_csv(here('data', 'age_grp_tab_plus65_5cond.csv'))
+count18plus5conds<-read_csv(here('data', 'age_grp_tab_plus18_5cond.csv'))
+count65plus5conds<-read_csv(here('data', 'age_grp_tab_plus65_5cond.csv'))
 pop<-read_csv(here('outputs', 'pop_calcs.csv'))
 plus18<-read_csv(here('data', 'plus18tab.csv'))
 plus65<-read_csv(here('data', 'plus65tab.csv'))
-plus18_4cond<-read_csv(here('data', 'plus18tab5cond.csv'))
-plus165_4cond<-read_csv(here('data', 'plus65tab5cond.csv'))
+plus18_5cond<-read_csv(here('data', 'plus18tab5cond.csv'))
+plus165_5cond<-read_csv(here('data', 'plus65tab5cond.csv'))
 
 # Data clean and merge ----------------------------------------------------
 
@@ -33,15 +33,15 @@ dt <- count18plus %>%
   mutate(dep_type=ifelse(type %in% c("IMD", "income"), type, NA)) %>% 
   fill(dep_type, .direction="down") %>% 
   rename(quintile=type) %>% 
-  left_join(count18plus4conds %>% 
+  left_join(count18plus5conds %>% 
               select(-1) %>% 
-              rename(counts4conds=count) %>% 
+              rename(counts5conds=count) %>% 
               mutate(dep_type=ifelse(type %in% c("IMD", "income"), type, NA)) %>% 
               fill(dep_type, .direction="down") %>% 
-              filter(!is.na(counts4conds)), by = c("quintile" = "type", "sex", "age_grp", "dep_type")) %>% 
+              filter(!is.na(counts5conds)), by = c("quintile" = "type", "sex", "age_grp", "dep_type")) %>% 
   mutate(rate19=100*(count/pop19)) %>% 
   mutate(rate20=100*(count/pop20)) %>% 
-  mutate(rate19_4conds=100*(counts4conds/pop19)) %>% 
+  mutate(rate19_5conds=100*(counts5conds/pop19)) %>% 
   filter(!is.na(rate19)) 
 
 
@@ -62,13 +62,13 @@ dep_extract<-function(raw=plus18,cond4=TRUE) {
 
 plus18<-dep_extract(plus18, cond4=FALSE)
 plus65<-dep_extract(plus65, cond4=FALSE)
-plus18_4cond<-dep_extract(plus18_4cond, cond4=TRUE)
-plus65_4cond<-dep_extract(plus165_4cond, cond4=TRUE)
+plus18_5cond<-dep_extract(plus18_5cond, cond4=TRUE)
+plus65_5cond<-dep_extract(plus165_5cond, cond4=TRUE)
 
-dt_desc<-plus18_4cond %>% 
-  mutate(conds="4conds", age_grp="18plus") %>% 
-  full_join(plus65_4cond %>% 
-              mutate(conds="4conds", age_grp="65plus")) %>% 
+dt_desc<-plus18_5cond %>% 
+  mutate(conds="5conds", age_grp="18plus") %>% 
+  full_join(plus65_5cond %>% 
+              mutate(conds="5conds", age_grp="65plus")) %>% 
   full_join(plus18 %>% 
               mutate(conds="9conds", age_grp="18plus")) %>% 
   full_join(plus65 %>% 
@@ -113,7 +113,7 @@ dt %>%
 #5 conditions 
 dt %>% 
   mutate(quintile=factor(quintile, levels=c(5:1), labels=c(1:5))) %>% 
-  ggplot(aes(x=age_grp, y=rate19_4conds, fill=quintile, colour=quintile))+
+  ggplot(aes(x=age_grp, y=rate19_5conds, fill=quintile, colour=quintile))+
   geom_bar(stat="identity", position="dodge") +
   facet_grid(cols=vars(sex), rows=vars(dep_type))+
   scale_fill_manual(values = pal) + 
@@ -128,7 +128,7 @@ dt %>%
 dt %>% 
   filter(plus65==1) %>% 
   mutate(quintile=factor(quintile, levels=c(5:1), labels=c(1:5))) %>% 
-  ggplot(aes(x=age_grp, y=rate19_4conds, fill=quintile, colour=quintile))+
+  ggplot(aes(x=age_grp, y=rate19_5conds, fill=quintile, colour=quintile))+
   geom_bar(stat="identity", position="dodge") +
   facet_grid(cols=vars(sex), rows=vars(dep_type))+
   scale_fill_manual(values = pal) + 
@@ -210,7 +210,7 @@ dt_tab<-dt %>%
   mutate(sex=factor(sex, levels=c("Males", "Females"))) %>% 
   mutate(age_grp=factor(age_grp, levels=c("18-39","35-39", "40-44","45-49", "50-54", 
                                           "55-59", "60-64","65-69","70-74", "75-79", "80-84", "85-89","90+"))) %>% 
-  select(dep_type, quintile, age_grp, sex, pop19, count, counts4conds) %>% 
+  select(dep_type, quintile, age_grp, sex, pop19, count, counts5conds) %>% 
   group_by(dep_type, quintile, age_grp) %>% 
   arrange(desc(sex))
 
